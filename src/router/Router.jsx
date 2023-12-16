3e5afafeeb96386b14198e508e23d996c2468f80
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-16 08:27:29 +0200

    still adding routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,6 +6,20 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 import Home from '../pages/Home'
+import PostDetail from '../pages/PostDetail'
+import Author from '../pages/Author'
+import AuthorPosts from '../pages/AuthorPosts'
+import categoryPosts from '../pages/categoryPosts'
+import createPosts from '../pages/createPosts'
+import Dashboard from '../pages/Dashboard'
+import DeletePost from '../pages/DeletePost'
+import EditPost from '../pages/EditPost'
+import ErrorPage from '../pages/ErrorPage'
+import Dashboard from '../pages/Dashboard'
+import Login from '../pages/Login'
+import Logout from '../pages/Logout'
+import Register from '../pages/Register'
+import UserProfile from '../pages/UserProfile'
 
 const Router = () => {
   return (
