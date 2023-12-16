c27bce208ad312285fd28fbbf89cd2e723187aba
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-16 08:23:13 +0200

    adding some routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+
+import Home from '../pages/Home'
 
 const Router = () => {
   return (
     <>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>     
     </>
   )
 }
