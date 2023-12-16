4f514c2546dc659d2074ae7c4d87d1b316c5668e
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-16 08:47:49 +0200

    still making the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <nav>
+      <div className="contaner nav-container">
+        <Link className='nav-logo' to={'/'}>
+          <h3>Prince <span>Dev</span></h3>
+        </Link>
+        <ul className="nav-menu">
+          <li><Link to={'/profile'}>Prince</Link></li>
+          <li><Link to={'/create'}>Create Posts</Link></li>
+          <li><Link to={'/authors'}>Authors</Link></li>
+          <li><Link to={'/logout'}>Log Out</Link></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
