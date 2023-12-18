2aa284bf1d10799bbc33f71e2112939b544a7d2b
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-18 21:44:29 +0200

    added some footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <ul className='footer-categories'>
+        <li><Link to={'/posts/categories/Education'}>Education</Link></li>
+        <li><Link to={'/posts/categories/Discovery'}>Discovery</Link></li>
+        <li><Link to={'/posts/categories/Agriculture'}>Agriculture</Link></li>
+        <li><Link to={'/posts/categories/Entertainment'}>Entertainment</Link></li>
+        <li><Link to={'/posts/categories/Art'}>Art</Link></li>
+        <li><Link to={'/posts/categories/Business'}>Business</Link></li>
+        <li><Link to={'/posts/categories/Investment'}>Investment</Link></li>
+        <li><Link to={'/posts/categories/Fashion'}>Fashion</Link></li>
+        <li><Link to={'/posts/categories/Weather'}>Weather</Link></li>
+        <li><Link to={'/posts/categories/Sports'}>Sports</Link></li>
+        <li><Link to={'/posts/categories/Science'}>Science</Link></li>
+        <li><Link to={'/posts/categories/Movie'}>Movie</Link></li>
+        <li><Link to={'/posts/categories/Music'}>Music</Link></li>
+      </ul>
+    </footer>
   )
 }
 
