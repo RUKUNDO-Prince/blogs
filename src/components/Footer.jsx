9c97031faa919469356d5751814b91bdfa4ca38d
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-18 22:13:37 +0200

    still adding some styles for the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,26 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      <ul className='footer-categories'>
-        <li><Link to={'/posts/categories/Education'}>Education</Link></li>
-        <li><Link to={'/posts/categories/Discovery'}>Discovery</Link></li>
-        <li><Link to={'/posts/categories/Agriculture'}>Agriculture</Link></li>
-        <li><Link to={'/posts/categories/Entertainment'}>Entertainment</Link></li>
-        <li><Link to={'/posts/categories/Art'}>Art</Link></li>
-        <li><Link to={'/posts/categories/Business'}>Business</Link></li>
-        <li><Link to={'/posts/categories/Investment'}>Investment</Link></li>
-        <li><Link to={'/posts/categories/Fashion'}>Fashion</Link></li>
-        <li><Link to={'/posts/categories/Weather'}>Weather</Link></li>
-        <li><Link to={'/posts/categories/Sports'}>Sports</Link></li>
-        <li><Link to={'/posts/categories/Science'}>Science</Link></li>
-        <li><Link to={'/posts/categories/Movie'}>Movie</Link></li>
-        <li><Link to={'/posts/categories/Music'}>Music</Link></li>
-      </ul>
+      <div className="container footer-container">
+        <ul className='footer-categories'>
+          <li><Link to={'/posts/categories/Education'}>Education</Link></li>
+          <li><Link to={'/posts/categories/Discovery'}>Discovery</Link></li>
+          <li><Link to={'/posts/categories/Agriculture'}>Agriculture</Link></li>
+          <li><Link to={'/posts/categories/Entertainment'}>Entertainment</Link></li>
+          <li><Link to={'/posts/categories/Art'}>Art</Link></li>
+          <li><Link to={'/posts/categories/Business'}>Business</Link></li>
+          <li><Link to={'/posts/categories/Investment'}>Investment</Link></li>
+          <li><Link to={'/posts/categories/Fashion'}>Fashion</Link></li>
+          <li><Link to={'/posts/categories/Weather'}>Weather</Link></li>
+          <li><Link to={'/posts/categories/Sports'}>Sports</Link></li>
+          <li><Link to={'/posts/categories/Science'}>Science</Link></li>
+          <li><Link to={'/posts/categories/Movie'}>Movie</Link></li>
+          <li><Link to={'/posts/categories/Music'}>Music</Link></li>
+        </ul>
 
-      <div className="footer-copyright">
-        <small> &copy; Copyright All Rights Reserved || Prince 2024</small>
+        <div className="footer-copyright">
+          <small> &copy; Copyright All Rights Reserved || Prince 2024</small>
+        </div>
       </div>
     </footer>
   )
