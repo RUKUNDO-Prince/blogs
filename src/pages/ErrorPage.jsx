9db7cc8fba43aa7f60e6af9800b09404655de5c2
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-19 17:42:30 +0200

    still making the errors page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ErrorPage = () => {
   return (
     <section>
-      
+      <div className="error-center">
+        <h2>Oops!</h2>
+        <h3>This page is not found😟!</h3>
+      </div>
     </section>
   )
 }
