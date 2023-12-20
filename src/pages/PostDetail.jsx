cf89e47109db9fb4ae0538715d7f97e3e95f51fc
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-20 23:35:56 +0200

    still editing the post details

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostAuthor from '../components/PostAuthor'
+import { Link } from 'react-router-dom'
 
 const PostDetail = () => {
   return (
@@ -7,6 +8,11 @@ const PostDetail = () => {
       <div className="container post-detail-container">
         <div className="post-detail-top">
           <PostAuthor />
+          <div className="post-detail-button">
+            <div className="post-detail-button">
+              <Link to={'/posts/werwer/edit'} className='btn btn-primary btn-sm'>Edit</Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
